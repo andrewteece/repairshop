c7967de3169782528e8166b276533b3751827b88
andrewteece <andrewteece@gmail.com>
2025-05-18 07:32:05 -0500

    FIX: slide animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 
 export default function Header() {
   return (
-    <header className='animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20'>
+    <header className='slide bg-background h-12 p-2 border-b sticky top-0 z-20'>
       <div className='flex h-8 items-center justify-between w-full'>
         <div className='flex items-center gap-2'>
           <NavButton href='/tickets' label='Home' icon={HomeIcon} />
